ac082d617272c3867d6c419751f811ddaf1e40ea
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-02-06 18:15:50 +0600

    Add movie rating and movie overview on card hover

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -10,17 +10,32 @@ interface Props {
   vote_average: number;
 }
 
-const Card: React.FC<Props> = ({ poster, title, year, overview }) => {
+const Card: React.FC<Props> = ({
+  poster,
+  title,
+  year,
+  overview,
+  vote_average,
+}) => {
+  let color = '';
+  if (vote_average >= 8) {
+    color = 'green';
+  } else if (vote_average >= 5) {
+    color = 'orange';
+  } else {
+    color = 'red';
+  }
+
+  const rounded_score = Math.round(vote_average * 100) / 100;
   return (
     <div className='movie'>
+      <span className={`rating ${color}`}>{rounded_score}</span>
       <img src={BASE_POSTER_URL + poster} alt={title} className='poster' />
       <div className='info'>
         <h4 className='title'>{title}</h4>
         <span className='year'>{year}</span>
       </div>
-      <div className='overview'>
-        <p>{overview}</p>
-      </div>
+      <div className='overview'>{overview}</div>
     </div>
   );
 };
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import React from 'react';
+import './Navbar.css';
 
 interface Props {
   changeCategory: (name: string) => void;
